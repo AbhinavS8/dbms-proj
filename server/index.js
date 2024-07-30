456a5390d2deb96074e8b46bfe2fab79ff0b006a
AbhinavS8 <abhinav.subr@gmail.com>
2024-07-30 08:47:08 +0530

    setting up sequelize

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@
 const express = require("express") //imports express module into variable
 const app = express() //creates instance of express in variable app
 
-app.listen(3001,
+app.listen(3001, () => { //begins the express server and listens for incoming connections on the given port numbeer
     console.log("server running..")
-) 
+}) 
+
